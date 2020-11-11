67c3f5b7a061bb276f744bf0ba67540352730136
pin0S <pete.g.lynch@gmail.com>
2020-11-11 13:59:03 +1100

    signout functionality working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,14 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -18,3 +23,5 @@ require("channels")
 
 import '../stylesheets/application'
 import './bootstrap_custom.js'
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'cards#index'
+  devise_for :users
 
   resources :cards
 
