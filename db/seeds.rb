e7bb4c6601c742e7dee49957b95591783b26508b
pin0S <pete.g.lynch@gmail.com>
2020-11-12 16:30:30 +1100

    seeds data updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# (1970..2021).each do |year|
-#     x = year
-#     y = year + 1
-#     z = Season.create(years: "#{x}-#{y}")
-#     z.save!
-# end
+(1970..2021).each do |year|
+    x = year
+    y = year + 1
+    z = Season.create(years: "#{x}-#{y}")
+    z.save!
+end
 
 # Faker::Sports::Basketball.team.each do |team|
 #     Team.create(team: team)
