e4a98a0805f2ab24f0cf456742c40426f9be7b3d
pin0S <pete.g.lynch@gmail.com>
2020-11-13 10:58:48 +1100

    user validation complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable #:trackable
 
+  validates :username, :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, :last_name, presence: true, length: { maximum: 50, 
+    too_long: "%{count} characters is the maximum allowed" } 
+
   has_many :ratings
-  has_many :cards
+  has_many :cards, dependent: :destroy
 end
