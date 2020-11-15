4f74c45d0133eb855648a199387f796dc28bb409
pin0S <pete.g.lynch@gmail.com>
2020-11-15 13:11:42 +1100

    backend for messaging done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
+
+  #root to landing page for the app
   root 'cards#index'
+
+  #devise user routes
   devise_for :users
 
+  #routes for main functions of the application
   resources :cards
 
+  #stripe payment routes
   get "payments/success", to: "payments#success"
 
+  #user personal account route
   get "/account/", to: "users#account"
 
+  #configure routes for messenger
+  resources :conversations, only: [:index, :create] do
+    resources :messages, only: [:index, :create]
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_024942) do
+ActiveRecord::Schema.define(version: 2020_11_15_003902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,10 +52,34 @@ ActiveRecord::Schema.define(version: 2020_11_10_024942) do
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "receiver_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "read"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.bigint "seller_id", null: false
+    t.bigint "buyer_id", null: false
+    t.bigint "card_id", null: false
+    t.index ["buyer_id"], name: "index_orders_on_buyer_id"
+    t.index ["card_id"], name: "index_orders_on_card_id"
+    t.index ["seller_id"], name: "index_orders_on_seller_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -107,6 +131,11 @@ ActiveRecord::Schema.define(version: 2020_11_10_024942) do
   add_foreign_key "cards", "seasons"
   add_foreign_key "cards", "teams"
   add_foreign_key "cards", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
+  add_foreign_key "orders", "cards"
+  add_foreign_key "orders", "cards", column: "seller_id"
   add_foreign_key "orders", "users"
+  add_foreign_key "orders", "users", column: "buyer_id"
   add_foreign_key "ratings", "users"
 end
