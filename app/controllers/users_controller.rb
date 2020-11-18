67fb871d8b4ffd660588f93d95d8f45ce24f505d
pin0S <pete.g.lynch@gmail.com>
2020-11-18 22:14:44 +1100

    styled image on show page to zoom

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
     def account
-        
         @cards = current_user.cards.with_attached_picture.paginate(:page => params[:page], :per_page=>8)
     end
 
